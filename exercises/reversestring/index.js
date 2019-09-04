38571f73bb50f257c5725b4ebcbe117ab4337bf9
tzone85 <tzone85@gmail.com>
2019-09-04 17:32:16 +0200

    just did a small code clearnup

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,6 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    const arr = str.split('');
-    arr.reverse();
-    return arr.join('');
+    return str.split('').reverse().join('');
 }
-reverse('mnscedi');
 module.exports = reverse;
