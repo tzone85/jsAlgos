b7725447f007a338ab758f0e56ccbd60fb773ec8
tzone85 <tzone85@gmail.com>
2019-09-05 20:29:21 +0200

    used character map to calculate the most frequently occuring character

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// character map
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charMap[char] = charMap[char]++ || 1;
+    }
+
+    // use "for in" for objects not "for of"
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
+
+
+// function maxChar(str) {
+//     const charMap = {};
+//
+//     for (let char of str) {
+//         if (charMap[char]) {
+//             charMap[char]++;
+//         } else {
+//             charMap[char] = 1;
+//         }
+//     }
+// }
