c55179dd2a7507525c70857346b95bd8209a0929
tzone85 <tzone85@gmail.com>
2019-09-05 19:58:07 +0200

    did the palindrome and reversing an integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,7 +7,23 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+function reverseInt(n) {
 
-function reverseInt(n) {}
+    return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+
+}
 
 module.exports = reverseInt;
+
+
+// my sol
+//
+// function reverseInt(n) {
+//
+//     if (Math.sign(n) === -1) {
+//         let temp = n*-1;
+//         return parseInt(temp.toString().split('').reverse().join('')) * -1;
+//     }
+//     return parseInt(n.toString().split('').reverse().join('');
+//
+// }
